c4be266ee445304fa3f73fb66137ba21e92c3b61
Elvira Danilenko <e.danilenko@a1qa.com>
2022-06-20 11:40:13 +0300

    adding project

---
+++ Api/Utils.cs
@@ -0,0 +1,23 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TestProjectWithDB.DataUtils;
+
+namespace TestProjectWithDB.Api
+{
+    public class Utils
+    {
+        public static bool IsDataJson(RestResponse data)
+        {
+            return data.ContentType.Contains("json");
+        }
+
+        public static bool IsDataSorted<T>(RestResponse data, Func<T, int> func)
+        {
+            List<T> SortedData = JsonUtils.GetModel<List<T>>(data).OrderBy(func).ToList();
+
+            return JsonUtils.GetModel<List<T>>(data).SequenceEqual(SortedData);
+        }
+    }
+}
---
+++ Resources/Pathes.cs
@@ -0,0 +1,9 @@
+﻿namespace TestProjectWithDB.Resources
+{
+    public class Pathes
+    {
+        public static readonly string generalPath = "https://www.nbrb.by/api/exrates/currencies";
+        public static readonly string pathForGettingPostList = "posts";
+        public static readonly string pathForGettingUserList = "users";
+    }
+}
